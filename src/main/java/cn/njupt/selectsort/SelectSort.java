f0a6083c8e66739c801e65343fbbc26fdd17cee7
Qin <245970829@qq.com>
2018-03-22 20:26:00 +0800

    选择排序

---
+++ src/main/java/cn/njupt/selectsort/SelectSort.java
@@ -0,0 +1,25 @@
+package cn.njupt.selectsort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序(O(N^2))
+ * @author Qin
+ */
+public class SelectSort {
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
