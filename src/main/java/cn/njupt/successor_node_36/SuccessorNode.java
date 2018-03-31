d6c4e0efebd5d9e70614f1028bb2f35653f49f6e
Qin <245970829@qq.com>
2018-03-31 09:58:05 +0800

    在二叉树中找到一个节点的后继节点

---
+++ src/main/java/cn/njupt/successor_node_36/SuccessorNode.java
@@ -0,0 +1,93 @@
+package cn.njupt.successor_node_36;
+
+/**
+ * 在二叉树中找到一个节点的后继节点
+ * 一个节点的后继节点就是它在中序遍历中的下一个节点
+ *
+ * 方法：
+ * 如果当前节点有右子树，则它的后继节点是他右子树的最左的节点
+ * 如果当前节点没有右子树，则当前节点在哪个节点的左子树中，后继节点就是哪个节点
+ * @author Qin
+ */
+public class SuccessorNode {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+        public Node parent;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node getSuccessorNode(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        if (node.right != null) {
+            return getLeftMost(node.right);
+        } else {
+            Node parent = node.parent;
+            while (parent != null && parent.left != node) {
+                node = parent;
+                parent = node.parent;
+            }
+
+            return parent;
+        }
+    }
+
+    public static Node getLeftMost(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(6);
+        head.parent = null;
+        head.left = new Node(3);
+        head.left.parent = head;
+        head.left.left = new Node(1);
+        head.left.left.parent = head.left;
+        head.left.left.right = new Node(2);
+        head.left.left.right.parent = head.left.left;
+        head.left.right = new Node(4);
+        head.left.right.parent = head.left;
+        head.left.right.right = new Node(5);
+        head.left.right.right.parent = head.left.right;
+        head.right = new Node(9);
+        head.right.parent = head;
+        head.right.left = new Node(8);
+        head.right.left.parent = head.right;
+        head.right.left.left = new Node(7);
+        head.right.left.left.parent = head.right.left;
+        head.right.right = new Node(10);
+        head.right.right.parent = head.right;
+
+        Node test = head.left.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.left.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.right.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.left.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.right; // 10's next is null
+        System.out.println(test.value + " next: " + getSuccessorNode(test));
+    }
+}
